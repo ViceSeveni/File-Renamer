3e7feb0e53a0919fd9c492ef01b81851a3ae9445
David <59756431+ViceSeveni@users.noreply.github.com>
2021-10-26 02:23:25 -0400

    General Changes
    Changed some of how it worked and removed Pyperclip use.

--- file_renamer.py
+++ file_renamer.py
@@ -1,45 +1,53 @@
 import os
-import pyperclip as clip
+
+def get_filetype(filename=''):
+    if filename == '':
+        filename = input('Enter A File Name: ')
+
+    period = '.'
+        
+    if period not in filename:
+        raise Exception('Invalid File Name. Must end with file type ".jpg"')
+        
+    if period in filename:
+        file_info = filename.split('.')
+        file_type = period + file_info[-1]
+        return file_type
 
 def rename_files():
 #! --The Folder To Be Searched Through    
-    search_folder = ''
+    search_folder = r""
+    
 #! --This Is The Text To Be Removed        
     tbr = []
+    
     file_list = os.listdir(search_folder)
-    for term in tbr:
-        for file in file_list:
+    
+    for file in file_list:
+        for term in tbr:
             term_slice = len(term)
             if file[0:term_slice].lower() == term.lower():
-                if file[-4:] == '.txt':
-                    print(f'Current name: {file[:-4]}')
-                if file[-5:] == '.epub':
-                    print(f'Current name: {file[:-5]}')
+                file_type = get_filetype(file)
+                type_slice = int(len(file_type))
+                file_name = file[: -type_slice]
+                print(f'Current Name: {file_name}')
+                    
+                    
                 old_path = f'{search_folder}{file}'
-                new_name = file[term_slice:]
+                new_name = file_name[term_slice:]
                 new_path = f'{search_folder}{new_name}'
+                
                 try:
                     os.rename(old_path , new_path)
                     print(f'New Name: {new_name}')
                     print('\n')
-                except:
-                    if file[-4:] == '.txt':
-                        clip.copy(file[term_slice:-4])
-                        manual_name = input('Enter a new name: ')
-                        manual_name = manual_name + '.txt'
-                        new_path = f'{search_folder}{manual_name}'
-                        os.rename(old_path , new_path)
-                        print(f'New Name: {manual_name}')
-                        print('\n')
-                    if file[-5:] == '.epub':
-                        clip.copy(file[term_slice:-5])
-                        manual_name = input('Enter a new name: ')
-                        manual_name = manual_name + '.epub'
-                        new_path = f'{search_folder}{manual_name}'
-                        os.rename(old_path , new_path)
-                        print(f'New Name: {manual_name}')
-                        print('\n')
-
                     
+                except:
+                    new_name = new_name + '-1'
+                    new_path = f'{search_folder}{new_name}'
+                    os.rename(old_path , new_path)
+                    print(f'New Name: {new_name}')
+                    print('\n')
+                        
 if __name__ == '__main__':
     rename_files()
